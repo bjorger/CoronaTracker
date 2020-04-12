82a5e8fa2456f000d12dc2a7ca5d650e24afccdd
bjorger <50590409+bjorger@users.noreply.github.com>
2020-04-12 18:05:44 +0200

    Added mobile header

---
+++ src/component/MobileHeader.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { slide as Menu } from 'react-burger-menu';
+import '../styles/MobileMenu.css';
+import { Link } from 'react-router-dom';
+
+export default class MobileMenu extends React.Component {
+	render() {
+		// NOTE: You also need to provide styles, see https://github.com/negomi/react-burger-menu#styling
+		return (
+			<Menu pageWrapId={'page-wrap'} outerContainerId={'root'}>
+				<div className="headerDiv">
+					<Link to="/">Home</Link>
+					<Link to="/Map">Map</Link>
+					<Link to="/Prevention">Prevention</Link>
+				</div>
+			</Menu>
+		);
+	}
+}
