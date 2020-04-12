ed8c0d143c7c71f126b3b631934655881feb7508
bjorger <50590409+bjorger@users.noreply.github.com>
2020-04-12 16:52:18 +0200

    Updates

---
+++ precache-manifest.e1a06c734076c440c22f3cb4ae8b0dd1.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a50c8d438e2050116ba89e4e831e2ee",
+    "url": "/CoronaTracker/index.html"
+  },
+  {
+    "revision": "83121ef816cb77501246",
+    "url": "/CoronaTracker/static/css/main.67ada23d.chunk.css"
+  },
+  {
+    "revision": "cd86087c4cb4e54dff09",
+    "url": "/CoronaTracker/static/js/2.abf6b78f.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/CoronaTracker/static/js/2.abf6b78f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83121ef816cb77501246",
+    "url": "/CoronaTracker/static/js/main.1193feb8.chunk.js"
+  },
+  {
+    "revision": "c3974c18fe4ed6375f8a",
+    "url": "/CoronaTracker/static/js/runtime-main.562ec301.js"
+  },
+  {
+    "revision": "1cd7ccd81278ed7e5c3b2f9ef5339f63",
+    "url": "/CoronaTracker/static/media/map.1cd7ccd8.png"
+  },
+  {
+    "revision": "5cc7d7ee406e32dbaa00a811be2956f2",
+    "url": "/CoronaTracker/static/media/oswald-v31-latin-200.5cc7d7ee.woff"
+  },
+  {
+    "revision": "6cabbbc8becd23d64515a8cd6b50ad8a",
+    "url": "/CoronaTracker/static/media/oswald-v31-latin-200.6cabbbc8.woff2"
+  },
+  {
+    "revision": "81dde49472b78aa6fb3f6886dde94cac",
+    "url": "/CoronaTracker/static/media/oswald-v31-latin-200.81dde494.eot"
+  },
+  {
+    "revision": "97a1f59401a0d12fb413df0e0ab7524f",
+    "url": "/CoronaTracker/static/media/oswald-v31-latin-200.97a1f594.ttf"
+  },
+  {
+    "revision": "b744f00031dd7562f0db86fc7e5abd74",
+    "url": "/CoronaTracker/static/media/oswald-v31-latin-200.b744f000.svg"
+  },
+  {
+    "revision": "34bd8be6a179b0547377d9399a45eaa0",
+    "url": "/CoronaTracker/static/media/oswald-v31-latin-300.34bd8be6.woff"
+  },
+  {
+    "revision": "650aac5096a5ffc76dc84e762acff206",
+    "url": "/CoronaTracker/static/media/oswald-v31-latin-300.650aac50.woff2"
+  },
+  {
+    "revision": "6ba8c3e1fb910cd545faf45c5306f20e",
+    "url": "/CoronaTracker/static/media/oswald-v31-latin-300.6ba8c3e1.eot"
+  },
+  {
+    "revision": "a240fb09ff42f2165fe3eef4c7cefb24",
+    "url": "/CoronaTracker/static/media/oswald-v31-latin-300.a240fb09.svg"
+  },
+  {
+    "revision": "c3a6d56debde68f045bbc4fb7be3944c",
+    "url": "/CoronaTracker/static/media/oswald-v31-latin-300.c3a6d56d.ttf"
+  },
+  {
+    "revision": "2cb2332cb77b34e5aa81eeff59b6f4c0",
+    "url": "/CoronaTracker/static/media/oswald-v31-latin-500.2cb2332c.eot"
+  },
+  {
+    "revision": "5937c6cb6a7ad1219eca4204286dd3b2",
+    "url": "/CoronaTracker/static/media/oswald-v31-latin-500.5937c6cb.woff"
+  },
+  {
+    "revision": "b296114f38cecb156ca8f8edc3e36654",
+    "url": "/CoronaTracker/static/media/oswald-v31-latin-500.b296114f.ttf"
+  },
+  {
+    "revision": "b7ce0f303c0587004ba9590124f3e21b",
+    "url": "/CoronaTracker/static/media/oswald-v31-latin-500.b7ce0f30.woff2"
+  },
+  {
+    "revision": "df6fe3bad375ddeeddf7fcbf2d86725b",
+    "url": "/CoronaTracker/static/media/oswald-v31-latin-500.df6fe3ba.svg"
+  },
+  {
+    "revision": "6a98169ce7d291e352b05f574181e0bc",
+    "url": "/CoronaTracker/static/media/oswald-v31-latin-600.6a98169c.woff"
+  },
+  {
+    "revision": "c86f7df28ab1432da37c2276638fcf09",
+    "url": "/CoronaTracker/static/media/oswald-v31-latin-600.c86f7df2.woff2"
+  },
+  {
+    "revision": "c8aca399820cc25aaee0ae5264a803fd",
+    "url": "/CoronaTracker/static/media/oswald-v31-latin-600.c8aca399.svg"
+  },
+  {
+    "revision": "f2fec2bb103f913f7ec4e16b1ed1d418",
+    "url": "/CoronaTracker/static/media/oswald-v31-latin-600.f2fec2bb.ttf"
+  },
+  {
+    "revision": "fc27a6664d49e66136529c425f5785b6",
+    "url": "/CoronaTracker/static/media/oswald-v31-latin-600.fc27a666.eot"
+  },
+  {
+    "revision": "1b8a94781c774bfaf851db3566952307",
+    "url": "/CoronaTracker/static/media/oswald-v31-latin-700.1b8a9478.svg"
+  },
+  {
+    "revision": "2580d38026677c2acc06fc528693f5d2",
+    "url": "/CoronaTracker/static/media/oswald-v31-latin-700.2580d380.eot"
+  },
+  {
+    "revision": "7c915161ba6ba5f3f0b00c8f0708eaa9",
+    "url": "/CoronaTracker/static/media/oswald-v31-latin-700.7c915161.woff2"
+  },
+  {
+    "revision": "c93625b557a32ebeb334b05dab8e4728",
+    "url": "/CoronaTracker/static/media/oswald-v31-latin-700.c93625b5.woff"
+  },
+  {
+    "revision": "eb7eb1a33ec652ec157e13d7ba6c9ebb",
+    "url": "/CoronaTracker/static/media/oswald-v31-latin-700.eb7eb1a3.ttf"
+  },
+  {
+    "revision": "0e88ec239d6256e2c889df2e3f0d51f2",
+    "url": "/CoronaTracker/static/media/oswald-v31-latin-regular.0e88ec23.woff"
+  },
+  {
+    "revision": "3f22213236dacb926063ad70c8dc946e",
+    "url": "/CoronaTracker/static/media/oswald-v31-latin-regular.3f222132.eot"
+  },
+  {
+    "revision": "dc872e68995b6f6a0a24f013ff750c50",
+    "url": "/CoronaTracker/static/media/oswald-v31-latin-regular.dc872e68.svg"
+  },
+  {
+    "revision": "f3655d2e126fb0891c94407ce82bea4b",
+    "url": "/CoronaTracker/static/media/oswald-v31-latin-regular.f3655d2e.woff2"
+  },
+  {
+    "revision": "fd2acbaf6dee51d0127b36a908fa50c1",
+    "url": "/CoronaTracker/static/media/oswald-v31-latin-regular.fd2acbaf.ttf"
+  }
+]);
